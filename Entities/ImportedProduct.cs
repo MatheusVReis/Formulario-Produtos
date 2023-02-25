b31ef1b37e31ff8cbf72910f33ba5a55d9b940d6
Matheus Vital <matheusvital48@gmail.com>
2023-02-25 11:33:35 -0300

    Criado formulario para dados do produto

---
+++ Entities/ImportedProduct.cs
@@ -0,0 +1,24 @@
+﻿namespace Course.Entities
+{
+    internal class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct() 
+        {
+        }
+        public ImportedProduct(string name, double price, double customFee)
+            : base(name, price)
+        {
+            CustomsFee = customFee;
+        }
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+        }
+        public override string PriceTag()
+        {
+            return Name + " $ " + TotalPrice() + $"(Customs fee: $ {CustomsFee})";
+        }
+    }
+}
